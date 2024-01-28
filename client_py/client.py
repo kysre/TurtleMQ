80067d49feebc9aa89f2a9180b5d45ec90cc78f9
PooyaY <59162706+pooyay@users.noreply.github.com>
2024-01-29 02:11:51 +0330

    Add client (#3)
    * client proto added
    
    * client push added
    
    * client ready for test
    
    * needs debug in pull
    
    * client almost done
    
    * unittest added
    
    * subscribe added
    
    * removed client_test.py
    
    * ready to merge
    
    * FIX client acknowledgment and subscribe, EDIT client to be asynchronous, ADD default datanode-server without any lock
    
    ---------
    
    Co-authored-by: maziar <maziar56bnd@gmail.com>

---
+++ client_py/client.py
@@ -0,0 +1,65 @@
+from typing import List
+import asyncio
+import logging
+
+import grpc
+# import google.protobuf.empty_pb2 as empty_pb2
+from google.protobuf import empty_pb2 as _empty_pb2
+
+from client_py import queue_pb2_grpc
+from client_py import queue_pb2
+
+HOST, PORT = "localhost", "8888"
+
+
+class QueueClient:
+    stub = None
+    HOST, PORT = "localhost", "8888"
+
+    @classmethod
+    def get_stub(cls, host: str, port: str):
+        if cls.stub is None:
+            channel = grpc.insecure_channel(f"{host}:{port}")
+            cls.stub = queue_pb2_grpc.QueueStub(channel)
+        return cls.stub
+
+    async def push(self, key: str, value: List[bytes]):
+        try:
+            stub = self.get_stub(HOST, PORT)
+
+            stub.Push(queue_pb2.PushRequest(key=key, value=value))
+
+        except grpc.RpcError as e:
+            print(f"Error in pushing: {e}.")
+
+    async def pull(self):
+        try:
+            stub = self.get_stub(HOST, PORT)
+            response = stub.Pull(f())
+            print(f"key and message: {response.key} - {response.value}")
+            await self.ack(response.key)
+            return response
+        except grpc.RpcError as e:
+            print(f"Error in pulling: {e}.")
+
+    async def ack(self, acknowledgement: str):
+        try:
+            stub = self.get_stub(HOST, PORT)
+            ack_request = queue_pb2.AcknowledgePullRequest(key=acknowledgement)
+            stub.AcknowledgePull(ack_request)
+            return True
+        except grpc.RpcError as e:
+            print(f"Error in acknowledgement: {e}")
+            return False
+
+    async def subscribe(self):
+        try:
+            while True:
+                await self.pull()
+        except grpc.RpcError as e:
+            print(f"Error in pulling: {e}.")
+        pass
+
+
+def f():
+    return _empty_pb2.Empty()
---
+++ client_py/test_client.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+from client_py.client import QueueClient
+from concurrent import futures
+import asyncio
+
+
+def sample_push_pull(client: QueueClient):
+    asyncio.run(client.push("test key", [b'test value']))
+    return asyncio.run(client.pull()).value
+
+
+class TestQueueClient(TestCase):
+    def setUp(self):
+        self.client = QueueClient()
+
+    def test_push_pull(self):
+        self.assertEqual(sample_push_pull(self.client), [b"test_value"])
+        # self.fail()
+
+    def test_ack(self):
+        ack_res = asyncio.run(self.client.ack("test key"))
+        self.assertTrue(ack_res)
+
+    def test_concurrent_push_without_order(self):
+
+        _futures = []
+
+        num_threads = 3
+
+        res = []
+
+        with futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+
+            for i in range(num_threads):
+                _futures.append(executor.submit(sample_push_pull, self.client))
+
+            for pulled_value in futures.as_completed(_futures):
+                try:
+                    res.append(pulled_value.result())
+                except Exception as e:
+                    print(f'Exception: {e}')
+
+        self.assertListEqual(res, [[b'test value'], [b'test value'], [b'test value']])
+
+    def test_subscribe(self):
+        self.fail()
