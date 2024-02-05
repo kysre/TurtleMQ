873007c33a5f456bfcaed62f4582f33b9058d60f
tandalalam <76206300+tandalalam@users.noreply.github.com>
2024-02-06 00:45:41 +0330

    Fix datanode discovery
    * Datanode-v1.0: Push, Pull, Ack with 1 partition. (need "load filesystems" functionality)
    
    * Add configs.Configmanager for ENV variables. Add cleaner to datanode server. Read message with index from filesystems.
    
    * rename test files.
    
    * Add leader notification
    
    * .
    
    * Add port to notification address. Add health.
    
    * Change Dockerfile Workdir and protos imports.
    
    * fix positional argument.
    
    * Add Pull logs.

--- datanode/src/datanode_server.py
+++ datanode/src/datanode_server.py
@@ -3,6 +3,8 @@ from concurrent import futures
 from google.protobuf import empty_pb2
 import protos.datanode_pb2_grpc as datanode_pb2_grpc
 import protos.datanode_pb2 as datanode_pb2
+import leader_protos.leader_pb2_grpc as leader_pb2_grpc
+import leader_protos.leader_pb2 as leader_pb2
 from configs.configs import ConfigManager
 from shared_partition import SharedPartitions
 from loguru import logger
@@ -20,8 +22,10 @@ class DataNode(datanode_pb2_grpc.DataNodeServicer):
 
     def Pull(self, request, context):
         try:
+            logger.info(f"received a pull message: {request}")
             message = self.shared_partition.pull()
             response = datanode_pb2.PullResponse(message=message)
+            logger.info(f"send a pull message: {message}")
             return response
         except grpc.RpcError as e:
             logger.exception(e)
@@ -31,7 +35,14 @@ class DataNode(datanode_pb2_grpc.DataNodeServicer):
     def AcknowledgePull(self, request, context):
         try:
             key = request.key
+            logger.info(f"received an acknowledge message: {key}")
             self.shared_partition.acknowledge(key)
+            return empty_pb2.Empty()
+        except grpc.RpcError as e:
+            logger.exception(f"Error in acknowledging. {e}")
+
+    def IsHealthy(self, request, context):
+        try:
             return empty_pb2.Empty()
         except grpc.RpcError as e:
             logger.exception(f"Error in acknowledging. {e}")
@@ -42,12 +53,25 @@ def serve():
     partitions_count = int(ConfigManager.get_prop('partition_count'))
     home_path = ConfigManager.get_prop('partition_home_path')
 
+    datanode_name = ConfigManager.get_prop('datanode_name')
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     datanode_pb2_grpc.add_DataNodeServicer_to_server(DataNode(partitions_count, home_path), server)
 
     server.add_insecure_port('[::]:' + port)
     server.start()
     logger.info('Server started, listening on ' + port)
+
+    # notify leader
+    try:
+        leader_host, leader_port = ConfigManager.get_prop('leader_host'), ConfigManager.get_prop('leader_port')
+        channel = grpc.insecure_channel(f"{leader_host}:{leader_port}")
+        stub = leader_pb2_grpc.LeaderStub(channel)
+        add_request = leader_pb2.AddDataNodeRequest(address=f'{datanode_name}:{port}')
+        stub.AddDataNode(add_request)
+    except grpc.RpcError as e:
+        print(f"Error in notifying leader: {e}.")
+
     server.wait_for_termination()
 
 
