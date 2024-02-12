0ccc791d6e99d2a0d1fcab9cb7a65285344c299c
Kasra Hajian <kasrahajian2002@gmail.com>
2024-02-12 04:38:46 +0330

    Cast input to number in scale workflow (#41)
    * Remove type check in scale script
    
    * Run script with bash

--- scale.sh
+++ scale.sh
@@ -7,13 +7,6 @@ else
         echo "1 argument required, $# provided."
         return 1
     fi
-    if ! [[ $1 =~ ^[0-9]+$ ]]; then
-        echo "Numeric argument required, $1 provided."
-        return 1
-    fi
-    if [ $1 -gt 10 -o $1 -lt 1 ]; then
-        echo "Value $1 is too big or too small."; return 1
-    fi
 
     python3 operation/diff-update-compose.py $1
     if [ ! $? -eq 0 ]; then
